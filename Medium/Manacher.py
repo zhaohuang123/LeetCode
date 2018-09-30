f48adaa70f8b63ee84e4817d3b85ee0f7aaffa89
huangzhao <zhaohuang132@qq.com>
2018-09-30 17:39:15 +0800

    rm something

--- Medium/Manacher.py
+++ Medium/Manacher.py
@@ -2,14 +2,28 @@
 
 class Solution:
     def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
         s = Solution.PreProcess(s)  #预处理
-        p = []  #p数组，保存回文长度
-        c = 0   #中心点位置，初始化为0
-        r = 0   #右边界位置，初始化为0
+        P = [0] * (len(s))  #p数组，保存回文长度
+        C = 0   #中心点位置，初始化为0
+        R = 0   #右边界位置，初始化为0
+        for i in range(1, len(s) - 1):
+            iMirror = 2*C - i
+            if iMirror < 0:
+                iMirror = 0
+            # 计算在当前以C为中心的的最大对称位置
+            P[i] = min(R - i, P[iMirror]) if R > i else 0
+            # 对边界进行扩展，利用循环对边界进行扩展
+            while (s[i - 1 - P[i]] == s[i + 1 + P[i]]):
+                P[i] += 1
+            # 计算新的C和R的位置
+            if (i + P[i] > R):
+                R = i + P[i]
+                C = i
+
+        MaxLen = max(P)
+        index = P.index(MaxLen)
+        return s[index - MaxLen: index + MaxLen + 1].replace('#', '')
+
 
     def PreProcess(s):
        """
@@ -22,6 +36,8 @@ class Solution:
 
 
 if __name__ == '__main__':
-    s = 'cbbd'
+    # s = 'cbbd'
+    # s = "babcbabcbaccba"
+    s = ""
     sl = Solution()
     print(sl.longestPalindrome(s))
